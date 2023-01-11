520b8135b4c2c924c4f988686731e745e4725e0b
Czarek Karczewski <keracz1997@gmail.com>
2023-01-11 12:41:26 +0100

    add median function

--- backend/nbp/helpers/statistical_measures.py
+++ backend/nbp/helpers/statistical_measures.py
@@ -1,3 +1,9 @@
 
-def median():
-    pass
+def median(numbers_array):
+    currency_prices = sorted(numbers_array)
+    index = len(currency_prices) // 2
+    
+    if len(currency_prices) % 2 != 0:
+        return currency_prices[index]
+    else:
+        return (currency_prices[index - 1] + currency_prices[index]) / 2
