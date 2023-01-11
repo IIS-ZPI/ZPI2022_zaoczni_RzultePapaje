ade41d1839e960a8ae2d9dafb4e4e07a0a60f654
Czarek Karczewski <keracz1997@gmail.com>
2023-01-11 14:31:12 +0100

    add standard_deviation function

--- backend/nbp/helpers/statistical_measures.py
+++ backend/nbp/helpers/statistical_measures.py
@@ -1,4 +1,6 @@
+import math
 test = [181, 187, 196, 196, 198, 203, 207, 211, 215, 123, 199]
+test2 = [7, 4, -2]
 
 def median(numbers_array):
     currency_prices = sorted(numbers_array)
@@ -25,3 +27,11 @@ def dominant(numbers_array):
             
     return dominant
 
+def standard_deviation(numbers_array):
+    mean = sum(numbers_array) / len(numbers_array)
+    dominator = list(map(lambda x: pow(x-mean,2), numbers_array))
+    variation = sum(dominator) / len(dominator)
+    return math.sqrt(variation)
+
+print(standard_deviation(test2))
+    
