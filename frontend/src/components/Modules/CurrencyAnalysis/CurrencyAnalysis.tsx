e668b48e14a5ea24bc353bd44da1c8108cb96ef4
Dudzinski-Robert <62355844+Dudzinski-Robert@users.noreply.github.com>
2023-01-12 22:59:21 +0100

    Update CurrencyAnalysis.tsx

--- frontend/src/components/Modules/CurrencyAnalysis/CurrencyAnalysis.tsx
+++ frontend/src/components/Modules/CurrencyAnalysis/CurrencyAnalysis.tsx
@@ -46,9 +46,43 @@ const CurrencyAnalysis = () => {
         ],
     };
 
+    const timePeriodName = ['Tydzień', '2 Tygodnie', 'Miesiąc', 'Kwartał', '6 Miesięcy', 'Rok']
+
+    const renderTimePeriodButton = () => {
+        return (
+            <div className='min-w-[90%] overflow-auto overflow-y-hidden flex justify-center'>
+                {timePeriodName.map((item, index) => {
+                    return (
+                        <button 
+                            className={`border-[2px] border-blue-500 p-[5px]
+                                        ${index === 0 ? 'rounded-l-lg' : 'border-l-0'}
+                                        ${index ===  timePeriodName.length - 1? 'rounded-r-lg' : ''}
+                                        ${selectedTime === index ? 'bg-blue-500' : ''}
+                                        `}
+                            onClick={() => changeSelectedTime(index)}
+                            key={index}
+                            >
+                            <div className={`font-medium text-xs sm:text-base ${selectedTime === index ? 'text-white' : 'text-blue-500'}`}>{item}</div> 
+                        </button>
+                    )
+                })}
+            </div>
+        )
+    }
+
+    const changeSelectedTime = (value: number) => {
+        setSelectedTime(value);
+    }
+
+    
+
     const fetchData = () => {
         setCurrencyData([12,3,56]);
         setLabelName(currencyCode)
+    }
+
+    const changeCurrency = (currencyCode: string) => {
+        setCurrencyCode(currencyCode);
     }
 
     useEffect(() => {
@@ -65,17 +99,18 @@ const CurrencyAnalysis = () => {
 
         <div className='grid lg:grid-cols-3 grid-cols-1 mt-[40px]'>
             <div className='lg:col-span-2 col-span-1 lg:row-start-1 row-start-0 mx-[40px] h-[500px] bg-white p-[80px] rounded-lg shadow'>
+                    <div className='flex justify-center mb-[20px]'>
+                        {renderTimePeriodButton()}
+                    </div>
                 <Bar options={options} data={data}/>
             </div>
             <div className='flex justify-center row-start-1 lg:row-start-0  mb-[30px]'>
                 <div className='bg-white w-[85%] max-h-[175px] p-[10px] pb-[20px] rounded-lg shadow flex  flex-col items-center'>
                     <div className='mb-[10px] text-lg font-medium'>Wybierz walutę</div>
                     <div className='flex justify-center lg:flex-col sm:flex-row flex-col items-center'>
-                        <CurrencyPicker countryCode='PLN'/>
+                        <CurrencyPicker countryCode='PLN' onChange={(c) => changeCurrency(c)}/>
                     </div>
-
-                    <Button text='Sprawdź'/>
-
+                    <Button text='Sprawdź' onClick={fetchData}/>
                 </div>
             </div>
         </div>
