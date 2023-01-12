2a4fcd38d900523e60ff3039e7961487ff7975e3
Dudzinski-Robert <62355844+Dudzinski-Robert@users.noreply.github.com>
2023-01-12 21:41:25 +0100

    Create countryFlag component

--- frontend/src/components/CountryFlag/CountryFlag.tsx
+++ frontend/src/components/CountryFlag/CountryFlag.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import ReactCountryFlag from 'react-country-flag'
 
-const CountryFlag = () => {
+interface props {
+    countryCode?: string,
+    size?: Number
+}
+
+const CountryFlag = ({countryCode = "US", size = 40}: props) => {
   return (
-    <div>CountryFlag</div>
+        <div className={`rounded-[50%] border border-stone-300 overflow-hidden shadow-md shadow-slate-400`} style={{width: `${size}px`, height: `${size}px`}}>
+            <ReactCountryFlag 
+                countryCode={countryCode} 
+                svg
+                style={{
+                    width: `${size}px`,
+                    height: `${size}px`,
+                    transform: 'scale(1.4)'
+                    }}
+            />
+        </div>
   )
 }
 
