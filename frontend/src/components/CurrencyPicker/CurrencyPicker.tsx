94a337a4191beb1b433d418ed931790e3a2a2010
Dudzinski-Robert <62355844+Dudzinski-Robert@users.noreply.github.com>
2023-01-12 21:43:35 +0100

    Styling picker

--- frontend/src/components/CurrencyPicker/CurrencyPicker.tsx
+++ frontend/src/components/CurrencyPicker/CurrencyPicker.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import CountryFlag from '../CountryFlag/CountryFlag'
 
 const CurrencyPicker = () => {
   return (
-    <div>CurrencyPicker</div>
+    <button 
+        className='border-[2px] border-stone-300 h-[60px] min-w-[200px] rounded-md flex items-center select-none'
+        >
+        <div className='ml-[10px]'>
+            <CountryFlag/>
+        </div>
+        <div className='text-left ml-[10px]'>
+            <div className='text-base font-medium'>{"PLN"}</div> 
+            <div className='text-xs text-gray-600'>{"ZŁoty"}</div>
+        </div>
+    </button>
   )
 }
 
