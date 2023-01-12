1ebb450263837420ce138b85327caebeb6f72c13
Dudzinski-Robert <62355844+Dudzinski-Robert@users.noreply.github.com>
2023-01-12 22:24:53 +0100

    Update Button.tsx

--- frontend/src/components/Button/Button.tsx
+++ frontend/src/components/Button/Button.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+interface props {
+    text: string,
+    onClick?: () => void
+}
 
-const Button = () => {
-  return (
-    <div>Button</div>
-  )
+const Button = ({text, onClick}: props) => {
+    return (
+        <button 
+            className='bg-blue-500 p-[5px] min-w-[100px] rounded-md mt-[20px]'
+            onClick={onClick}
+            >
+            <div className='text-white text-sm'>
+                {text}
+            </div>
+        </button>
+    )
 }
 
 export default Button
