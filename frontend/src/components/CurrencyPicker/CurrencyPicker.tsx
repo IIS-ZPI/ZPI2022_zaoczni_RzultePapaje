13cae2e913c70a7f2a4c73430505b86de7b7435c
Dudzinski-Robert <62355844+Dudzinski-Robert@users.noreply.github.com>
2023-01-12 21:47:06 +0100

    Init new states

--- frontend/src/components/CurrencyPicker/CurrencyPicker.tsx
+++ frontend/src/components/CurrencyPicker/CurrencyPicker.tsx
@@ -1,20 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Currency } from '../../util/CurrencyData';
 import CountryFlag from '../CountryFlag/CountryFlag'
 
 const CurrencyPicker = () => {
-  return (
-    <button 
-        className='border-[2px] border-stone-300 h-[60px] min-w-[200px] rounded-md flex items-center select-none'
-        >
-        <div className='ml-[10px]'>
-            <CountryFlag/>
-        </div>
-        <div className='text-left ml-[10px]'>
-            <div className='text-base font-medium'>{"PLN"}</div> 
-            <div className='text-xs text-gray-600'>{"ZŁoty"}</div>
-        </div>
-    </button>
-  )
+    const [inputText, setInputText] = useState<string>("");
+    const [filteredCurrency, setFilteredCurrency] = useState<Currency[]>([]);
+    const [selectedCurrency, setCurrency] = useState<Currency>();
+    const [isOpen, setOpen] = useState<boolean>(false);
+
+    return (
+        <button 
+            className='border-[2px] border-stone-300 h-[60px] min-w-[200px] rounded-md flex items-center select-none'
+            >
+            <div className='ml-[10px]'>
+                <CountryFlag/>
+            </div>
+            <div className='text-left ml-[10px]'>
+                <div className='text-base font-medium'>{"PLN"}</div> 
+                <div className='text-xs text-gray-600'>{"ZŁoty"}</div>
+            </div>
+        </button>
+    )
 }
 
 export default CurrencyPicker
